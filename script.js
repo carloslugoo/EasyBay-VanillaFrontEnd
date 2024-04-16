bc81e39fd1b9fd737d78fd74355f0cd1cd3d9d0d
Carlos Lugo <112581880+carloslugoo@users.noreply.github.com>
2024-04-16 00:10:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function buttonAccept(element){
+    var cookies = document.getElementById("cookiesDiv");
+    cookies.style.display = "none";
+}
+function cambiarImagen(elemento) {
+    elemento.src = "./img/succulents-2.jpg"; 
+}
+function imagenOrinigal(elemento) {
+    elemento.src = "./img/succulents-1.jpg"; 
+}
